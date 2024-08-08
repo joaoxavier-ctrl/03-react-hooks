7fe8094b358c0b7eeb674d6bd7d12745483afe6b
JoaoXavierWin <joao.xavier21@gmail.com>
2024-08-08 15:26:19 -0300

    customHooks-parte1-finalizada

---
+++ 0306-customHooks/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import useLocalStorage from './useLocalStorage';
+
+const App = () => {
+  const [produto, setProduto] = useLocalStorage('produto', '');
+
+  function handleClick({target}){
+    setProduto(target.innerText);
+  }
+
+  return <div>
+    <button onClick={handleClick}>notebook</button>
+    <button onClick={handleClick}>smartphone</button>
+  </div>;
+}
+
+export default App;
